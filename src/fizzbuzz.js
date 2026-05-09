29558b9278c9b943790aa4bf762aaa6195952eeb
Dylan Missael <dylanmissael14@gmail.com>
2026-05-09 17:41:00 -0600

    Prcatica FizzBuzz terminada

--- src/fizzbuzz.js
+++ src/fizzbuzz.js
@@ -1,7 +1,16 @@
 export function fizzbuzz(){
-  //tu código va aqui
+  for(let i = 1; i <= 100; i++) {
+    if(i % 3 === 0 && i % 5 === 0) {
+      console.log('FizzBuzz');
+    } else if(i % 5 === 0) {
+      console.log('Buzz');
+    }else if(i % 3 === 0) {
+      console.log('Fizz');
+    } else {
+      console.log(i);
+    }
+  }
  
   
 }
-
 fizzbuzz();
